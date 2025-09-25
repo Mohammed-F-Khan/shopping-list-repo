3e3c1a53b255c3585511605c0dfaa0f1f14a8db0
MohammedKhan <mohammedk20259@gmail.com>
2025-09-25 13:53:51 -0400

    intial files

---
+++ src/main/java/com/pluralsight/Shoppinglist.java
@@ -0,0 +1,16 @@
+package com.pluralsight;
+
+public class Shoppinglist {
+    public static void main(String[] args) {
+        System.out.println("apricot");
+        System.out.println("apple");
+        System.out.println("bannana");
+        System.out.println("eggs");
+        System.out.println("milk");
+        System.out.println("chicken");
+        System.out.println("tomatoes");
+        System.out.println("soda");
+        System.out.println("juice");
+        System.out.println("butter");
+    }
+}
